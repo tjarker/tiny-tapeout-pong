f2f905bbbd7c6cbd77abb0e99da480cd5be2d1e7
Martin Schoeberl <martin@jopdesign.com>
2024-04-08 16:45:31 +0200

    Search for the linter issue

--- src/tt_um_chisel_template.v
+++ src/tt_um_chisel_template.v
@@ -17,9 +17,9 @@ module tt_um_chisel_template (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0;
+  // assign uio_oe  = 0;
     wire reset = !rst_n;
     // Just wrap the Chisel generated Verilog
 
